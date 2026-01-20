604c025e242b814e099fdbe7a71d6ed7b4e0f28d
johnsonxd-lab <johnsonxd@gmail.com>
2026-01-20 16:48:42 +0800

    Update app.py

--- app.py
+++ app.py
@@ -163,8 +163,8 @@ if not df.empty:
     metrics_html = f'''
     <div style="
         position: fixed; 
-        top: 50px; 
-        left: 25%; 
+        top: 60px; 
+        left: 50%; 
         transform: translateX(-50%);
         z-index: 9999; 
         background-color: rgba(20, 20, 20, 0.7); 
@@ -304,6 +304,7 @@ if not df.empty:
 
 else:
     st.error("目前無法取得台電資料，請稍後重試。")
+
 
 
 
